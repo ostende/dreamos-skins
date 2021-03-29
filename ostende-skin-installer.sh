735e4cf5f6f47140ff18ed2b53a9dfdad1a51ff7
Redouane <redouaneelrhachi@gmail.com>
2021-03-29 13:18:12 +0200

    rebuild

--- ostende-skin-installer.sh
+++ ostende-skin-installer.sh
@@ -7,9 +7,9 @@ echo "********** Downlaod and Install (( Zombi-Shadow-FHD Skin Mod Ostende)) ***
 echo ""
 wget --no-check-certificate "https://github.com/ostende/dreamos-skins/archive/refs/heads/main.zip" -O /tmp/ostende-dreamskins-main.zip > /dev/null 2>&1
 unzip /tmp/ostende-dreamskins-main.zip -d /tmp/ > /dev/null 2>&1
-cp -rf /tmp/ostende-dreamskins-main/ostende-dreamskins/Zombi-Shadow-FHD /usr/share/enigma2/Zombi-Shadow-FHD > /dev/null 2>&1
-cp -rf /tmp/ostende-dreamskins-main/ostende-dreamskins/renderers/* /usr/lib/enigma2/python/Components/Renderer/ > /dev/null 2>&1
-rm -rf /tmp/ostende-dreamskins* > /dev/null 2>&1
+cp -rf /tmp/dreamos-skins-main/ostende-dreamskins/Zombi-Shadow-FHD /usr/share/enigma2/Zombi-Shadow-FHD > /dev/null 2>&1
+cp -rf /tmp/dreamos-skins-main/ostende-dreamskins/renderers/* /usr/lib/enigma2/python/Components/Renderer/ > /dev/null 2>&1
+rm -rf /tmp/dreamos-skins-main* > /dev/null 2>&1
 rm -f /tmp/ostende-dreamskins-main.zip > /dev/null 2>&1
 
 echo "... install finish"
